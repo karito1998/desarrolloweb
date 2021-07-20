4ac32d6ce0d421cc6ce0df5b805249698abfe745
Karol Arciniegas <karol.arciniegas@mi.com.co>
2021-07-19 20:19:43 -0500

    sprint 2

--- app.js
+++ app.js
@@ -4,7 +4,48 @@ new Vue ({
   data () {
     return{
       name : 'Bitcoin',
-      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png'
+      symbol: 'BTC',
+      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 10,
+      price:8200,
+      color:'f4f4f4',
+      value:0, 
+      pricesWitgDays:[
+        {day: 'Lunes', value: 8400},
+        {day: 'Martes', value: 7900},
+        {day: 'Miercoles', value: 8200},
+        {day: 'Jueves', value: 9000},
+        {day: 'Viernes', value: 9400},
+        {day: 'Sabado', value: 10000},
+        {day: 'Domingo', value: 10200},
+      ],
+
+      showPrices: false
+    }
+  },
+  computed:{
+    title(){
+      return `${this.name} - ${this.symbol}`
+    },
+    convertedValue(){
+      if(!this.value){
+        return 0
+      }
+      return this.value / this.price
+    }
+  },
+
+  watch:{
+    showPrices (newVal, oldVal) {
+      console.log(newVal, oldVal)
+
+    }
+  },
+
+  methods:{
+    toggleShowPrices (){
+      this.showPrices = !this.showPrices
+      this.color = this.color.split('').reverse().join('')
     }
   }
 })
