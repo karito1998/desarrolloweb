48ccac7649f8ee8e442582ef49695e8b2e7f60b2
Karol Arciniegas <karol.arciniegas@mi.com.co>
2021-08-03 15:21:36 -0500

    sprint 3

--- app.js
+++ app.js
@@ -1,15 +1,76 @@
+Vue.component('CoinDetail',{
+  props: ['coin'],
+  data (){
+    return {
+      showPrices: false,
+      value: 0  }
+  },
+  methods:{
+    toggleShowPrices (){
+      this.showPrices = !this.showPrices
+      this.$emit('change-color', this.showPrices ? 'FF96C8' : '3D3D3D')  }
+  },
+  computed:{
+    title(){
+      return `${this.coin.name} - ${this.coin.symbol}` },
+    convertedValue(){
+      if(!this.value){ return 0 }
+      return this.value / this.coin.price
+    }
+  },
+  created (){
+    console.log('Created CoinDetail...')
+  },
+
+  mounted (){
+    console.log('Mounted CoinDetail...')
+  },
+
+  template:`
+  <div>
+    <img 
+        v-on:mouseover="toggleShowPrices"
+        v-on:mouseout="toggleShowPrices"
+        v-bind:src="coin.img" v-bind:alt= "coin.name">
+    <h1 v-bind:class ="coin.changePercent> 0 ? 'green' : 'red'">
+        {{title}}
+        <span v-if = "coin.changePercent > 0" >👍 </span>
+        <span v-else-if = "coin.changePercent < 0" >👎 </span>
+        <span v-else>🤘</span>
+        <span v-on:click="toggleShowPrices">
+          {{showPrices ? '😊': '😁'}}</span>
+    </h1>
+    <input type="number" v-model="value">
+    <span>{{convertedValue}}</span>
+
+    <slot name = "text"></slot>
+    <slot name = "link"></slot>
+
+    <ul v-show= showPrices>
+      <li 
+        class ="uppercase"
+        v-bind:class="{ 
+          orange: p.value == coin.price, 
+          red: p.value < coin.price, 
+          green: p.value > coin.price }"
+        v-for = "(p,i) in coin.pricesWitgDays" v-bind:key="p.day">
+        {{i}} - {{p.day}} - {{p.value}}
+      </li>
+    </ul>
+  </div>`
+})
+
 new Vue ({
   el: '#app',
 
   data () {
     return{
+    btc:{
       name : 'Bitcoin',
       symbol: 'BTC',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: 10,
       price:8200,
-      color:'f4f4f4',
-      value:0, 
       pricesWitgDays:[
         {day: 'Lunes', value: 8400},
         {day: 'Martes', value: 7900},
@@ -19,33 +80,24 @@ new Vue ({
         {day: 'Sabado', value: 10000},
         {day: 'Domingo', value: 10200},
       ],
-
-      showPrices: false
-    }
-  },
-  computed:{
-    title(){
-      return `${this.name} - ${this.symbol}`
     },
-    convertedValue(){
-      if(!this.value){
-        return 0
-      }
-      return this.value / this.price
-    }
-  },
-
-  watch:{
-    showPrices (newVal, oldVal) {
-      console.log(newVal, oldVal)
+ 
+      color:'f4f4f4',
 
     }
   },
+
 
   methods:{
-    toggleShowPrices (){
-      this.showPrices = !this.showPrices
-      this.color = this.color.split('').reverse().join('')
+    updateColor (color){
+      this.color = color || this.color.split('').reverse().join('')
     }
-  }
+  },
+  created (){
+    console.log('Created ...')
+  },
+
+  mounted (){
+    console.log('Mounted ...')
+  },
 })
